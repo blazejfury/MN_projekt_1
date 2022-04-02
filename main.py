664f0507e59b5b600aa3e8e051b209c345558b36
Błażej Tez <blaz.tez@gmail.com>
2022-04-02 21:42:55 +0200

    create_table() function added - works correct calculate_ema() rebuilt, now accepts slices added docstrings for both functions

--- main.py
+++ main.py
@@ -14,29 +14,76 @@ class Account:
 
 
 def load_data():
+    """
+    loads data from csv file, only opening prices are taken
+    returns a dataframe containing two columns: 'Date' and 'Zamkniecie' (closing price)
+    """
     df = pd.read_csv('historical.csv')
-    print(df.head())
     df.drop('Wolumen', axis='columns', inplace=True)
-    df.drop('Zamkniecie', axis='columns', inplace=True)
+    df.drop('Otwarcie', axis='columns', inplace=True)
     df.drop('Najnizszy', axis='columns', inplace=True)
     df.drop('Najwyzszy', axis='columns', inplace=True)
-    print(df.head())
     return df
 
 
-def calculate_ema(df: pd.DataFrame, n: int) -> float:
-    alpha = 2 / (n + 1)
-    return alpha
+def create_table(df: pd.core.frame.DataFrame):
+    """
+    creates appropriate slices for calculate_ema function,
+    passes them to calculate_ema, and saved in df Dataframe.
+    :param df: a pandas dataframe of size(X, ) containing historical prices of stock for calculations
+    :return: None, the changes are made inplace for better memory management
+    """
+
+    short_term_periods = 11
+    long_term_periods = 25
+    signal_periods = 8
+    prices = df['Zamkniecie']
+    ema_12 = np.zeros(prices.shape[0]-long_term_periods, dtype='float64')
+    ema_26 = np.zeros(prices.shape[0]-long_term_periods, dtype='float64')
+    macd = np.zeros(prices.shape[0]-long_term_periods, dtype='float64')
+    signal = np.zeros(prices.shape[0]-long_term_periods-signal_periods, dtype='float64')
+
+    for i, k in enumerate(ema_12):
+        ema_12[i] = calculate_ema(prices[i:i+short_term_periods])
+        ema_26[i] = calculate_ema(prices[i:i+long_term_periods])
+        macd[i] = ema_26[i] - ema_12[i]
+
+    for i, k in enumerate(signal):
+        signal[i] = calculate_ema(macd[i:i+signal_periods])
+
+    df.drop(df.index[0:25], inplace=True)
+    df['ema_12'] = ema_12
+    df['ema_26'] = ema_26
+    df['macd'] = ema_12 - ema_26
+    df.drop(df.index[0:8], inplace=True)
+    df['signal'] = signal
+    print(df)
+
+
+def calculate_ema(period: pd.core.frame.DataFrame) -> float:
+    """    
+    :param period: a slice of pandas Dataframe containing data from which to calculate the EMA
+    :return: calculated value of EMA
+    """"
+    alpha = 2 / (len(period) + 1)
+    nominator = 0
+    denominator = 0
+    for i, k in enumerate(period):
+        nominator = nominator + (((1 - alpha)**i) * k)
+        denominator = denominator + ((1-alpha)**i)
+    assert denominator != 0
+    return nominator/denominator
 
 
 def main():
     account = Account(0, 1000)
     df = load_data()
+    print(df.head())
+    create_table(df)
+    
 
 
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
